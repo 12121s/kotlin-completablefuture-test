a941e8ce18f90c4dcb87550e4404bbc16869fe9d
mr_kim <rofalfl42@gmail.com>
2022-08-15 23:49:32 +0900

    **수정내역**: 1. 최초 커밋 **프로젝트** : CompletableFutureTest **작성자** : illi

---
+++ CompletableFutureTest/src/main/kotlin/CompletableFutureTest.kt
@@ -0,0 +1,48 @@
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.Executor
+import java.util.concurrent.TimeUnit
+
+fun main(args: Array<String>) {
+    /* given */
+    val message = "Hello";
+    val messageFuture = CompletableFutureTest().sayMessage(message)
+
+    /* when */
+    try {
+        val result = messageFuture.get(5, TimeUnit.SECONDS)
+        println("result= $result")
+    } catch (e: Exception) {
+        println("timeout=  ${e.message}")
+    }
+}
+
+class CompletableFutureTest {
+
+    fun sayMessage(message: String) : CompletableFuture<String> {
+//        sleepOneSecond()
+
+        val future = CompletableFuture<String>()
+
+        /*CompletableFuture.delayedExecutor(1, TimeUnit.SECONDS).execute {
+            val saidMessage = "Say $message"
+            println("Said Message = {$saidMessage}")
+            future.complete(saidMessage)
+        }*/
+        val async = CompletableFuture.runAsync {
+            val saidMessage = "Say $message"
+            println("Said Message = {$saidMessage}")
+            future.complete(saidMessage)
+        }
+        return future
+    }
+
+    private fun sleepOneSecond() {
+        try {
+            println("start to sleep 1 second.")
+            Thread.sleep(1000)
+            println("end to sleep 1 second.")
+        } catch (e :InterruptedException) {
+            throw IllegalStateException()
+        }
+    }
+}
