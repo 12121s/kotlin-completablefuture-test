8b65520192e5e37b31ad8136a8adf7c91c18878e
mr_kim <rofalfl42@gmail.com>
2022-08-22 21:24:34 +0900

    **수정내역**: CompletableFuture 테스트 코드 수정 **프로젝트** : CompletableFutureTest **작성자** : illi

--- CompletableFutureTest/src/main/kotlin/CompletableFutureTest.kt
+++ CompletableFutureTest/src/main/kotlin/CompletableFutureTest.kt
@@ -21,25 +21,27 @@ class CompletableFutureTest {
     fun sayMessage(message: String) : CompletableFuture<String> {
 //        sleepOneSecond()
 
+
         val future = CompletableFuture<String>()
 
-        /*CompletableFuture.delayedExecutor(1, TimeUnit.SECONDS).execute {
-            val saidMessage = "Say $message"
-            println("Said Message = {$saidMessage}")
-            future.complete(saidMessage)
-        }*/
-        val async = CompletableFuture.runAsync {
+        CompletableFuture.delayedExecutor(5, TimeUnit.SECONDS).execute {
             val saidMessage = "Say $message"
             println("Said Message = {$saidMessage}")
             future.complete(saidMessage)
         }
+        /*val async = CompletableFuture.runAsync {
+            sleepOneSecond()
+            val saidMessage = "Say $message"
+            println("Said Message = {$saidMessage}")
+            future.complete(saidMessage)
+        }*/
         return future
     }
 
     private fun sleepOneSecond() {
         try {
             println("start to sleep 1 second.")
-            Thread.sleep(1000)
+            Thread.sleep(6000)
             println("end to sleep 1 second.")
         } catch (e :InterruptedException) {
             throw IllegalStateException()
